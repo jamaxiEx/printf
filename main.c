047a8401c6ed48d05231582d5b8aefb12024a82f
jamaxiEx <jamaxiex@outlook.com>
2023-04-28 00:04:43 +0100

    Fixes

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success, error code otherwise
+ */
+int main(void)
+{
+_printf("%.6s\n", "Best School !");
+return (0);
+}
