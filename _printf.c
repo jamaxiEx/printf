b3b3f69dea6f45fd7541358d102c0d6f6499b553
Sochima <verichima@yahoo.com>
2023-05-01 14:40:31 +0100

    Update _printf.c
    More updates on the printf file

--- _printf.c
+++ _printf.c
@@ -10,7 +10,7 @@ Return: The number of characters printed to stdout.
 
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
+	int a, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
@@ -20,25 +20,25 @@ int _printf(const char *format, ...)
 
 	va_start(list, format);
 
-	for (i = 0; format && format[i] != '\0'; i++)
+	for (a = 0; format && format[i] != '\0'; a++)
 	{
-		if (format[i] != '%')
+		if (format[a] != '%')
 		{
-			buffer[buff_ind++] = format[i];
+			buffer[buff_ind++] = format[a];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
+			/* write(1, &format[a], 1);*/
 			printed_chars++;
 		}
 		else
 		{
 			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
+			flags = get_flags(format, &a);
+			width = get_width(format, &a, list);
+			precision = get_precision(format, &a, list);
+			size = get_size(format, &a);
+			++a;
+			printed = handle_print(format, &a, list, buffer,
 				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
