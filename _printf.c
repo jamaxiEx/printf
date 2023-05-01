bb448823d70c8b7c86999845433b3bb750e2d895
Rising Felix <jamaxiex@outlook.com>
2023-05-01 14:35:21 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -3,10 +3,11 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
- */
+_printf - A function that prints formatted output to stdout.
+@format: A string that specifies the format of the output.
+Return: The number of characters printed to stdout.
+*/
+
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
